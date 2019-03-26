84c6f781004983f0df935aa562d25ef847fe8c57
Chepell <av.voytenko@gmail.com>
2019-03-27 00:35:29 +0300

    4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -66,6 +66,14 @@ public class Controller {
             resultLabel.setText(phonePattern);
         });
 
+        CountryPhone countryPhone = countryMap.get(choiceBox.getValue());
+        int firstSpace = countryPhone.getFirstSpace();
+        int secondSpace = countryPhone.getSecondSpace();
+        int firstDash = countryPhone.getFirstDash();
+        int secondDash = countryPhone.getSecondDash();
+        int fullPhoneLength = countryPhone.getFullPhoneLength();
+        String countryCode = countryPhone.getCountryCode();
+
         phoneField.textProperty().addListener((observable, oldValue, newValue) -> {
 //            if (!newValue.matches("[0-9,\\+,\\s,-]*")
 //                    || newValue.length() > countryMap.get(choiceBox.getValue()).getFullPhoneLength()) {
@@ -75,8 +83,14 @@ public class Controller {
             // "+7 ___ ___-__-__");
 
 
-            if (!newValue.matches("^\\+?\\d*\\s?\\d*\\s?\\d*-?\\d*-?\\d*$")
-                    || newValue.length() > countryMap.get(choiceBox.getValue()).getFullPhoneLength()
+//            if (!newValue.matches("^\\+?\\d*\\s?\\d*\\s?\\d*-?\\d*-?\\d*$")
+//            if (!newValue.matches("(^\\+\\d?\\s?(\\d{3})?(\\s?(\\d{3})?)?-?(\\d{2})?-?\\d{0,2}$)?")
+//            if (!newValue.matches("^(\\+(\\d(\\s(\\d{0,3}(\\s(\\d{0,3}(-(\\d{0,2}(-(\\d{0,2})?)?)?)?)?)?)?)?)?)?$")
+            if (!newValue
+                    .matches("^(\\+(\\d(\\s(\\d(\\d(\\d(\\s(\\d(\\d(\\d(-(\\d(\\d(-(\\d(\\d)?)?)?)?)?)?)?)?)?)?)?)" +
+                            "?)?)?)" +
+                            "?)?$")
+//                    || newValue.length() > countryMap.get(choiceBox.getValue()).getFullPhoneLength()
             ) {
                 phoneField.setText(oldValue);
             }
@@ -87,16 +101,17 @@ public class Controller {
             String text = phoneField.getText();
             int length = phoneField.getLength();
 
-            CountryPhone countryPhone = countryMap.get(choiceBox.getValue());
-            int secondSpace = countryPhone.getSecondSpace();
-            int firstDash = countryPhone.getFirstDash();
-            int secondDash = countryPhone.getSecondDash();
-            int fullPhoneLength = countryPhone.getFullPhoneLength();
-            String countryCode = countryPhone.getCountryCode();
+//            CountryPhone countryPhone = countryMap.get(choiceBox.getValue());
+//            int secondSpace = countryPhone.getSecondSpace();
+//            int firstDash = countryPhone.getFirstDash();
+//            int secondDash = countryPhone.getSecondDash();
+//            int fullPhoneLength = countryPhone.getFullPhoneLength();
+//            String countryCode = countryPhone.getCountryCode();
             KeyCode key = keyEvent.getCode();
 
             System.out.println("len : " + length);
-            System.out.println("key code: " + key.getChar());
+            System.out.println("key code: " + key.getName());
+            System.out.println("secondSpace: " + secondSpace);
 
             // если ввожу цифры, то форматирую строку в нужных местах дополняя пробелами и тире
             if (key.isDigitKey() && !text.isEmpty()) {
@@ -113,48 +128,52 @@ public class Controller {
                 phoneField.positionCaret(length);
             }
 
-            // если удаляю последний символ слева, то если нахожусь на месте расделителей маски их тоже удаляю
-            if (key == KeyCode.BACK_SPACE) {
-                if (length == secondSpace) {
-                    onBackSpaceFormatField(text, secondSpace);
-                } else if (length == firstDash) {
-                    onBackSpaceFormatField(text, firstDash);
-                } else if (length == secondDash) {
-                    onBackSpaceFormatField(text, secondDash);
-                }
-            }
 
-            // сразу удаляю введенные вручную не в нужных местах символы
-//            if (length < fullPhoneLength && length != 0
-//                    && (
-//                    key == KeyCode.ADD ||
-//                            key == KeyCode.SUBTRACT
-//                            || key == KeyCode.MINUS
-//                            || key == KeyCode.SPACE
-//                            || key.isWhitespaceKey())) {
-//                text = text.substring(0, length - 1);
+//            if (key != KeyCode.BACK_SPACE) {
+//                if (length == secondSpace) {
+//                    phoneField.appendText(" ");
+//                } else if (length == firstDash || length == secondDash) {
+//                    phoneField.appendText("-");
+//                }
 //                length = text.length();
 //                phoneField.clear();
 //                phoneField.setText(text);
 //                phoneField.positionCaret(length);
 //            }
 
-            // если введена не цифра то удаляю ее
-//            if (!key.isDigitKey() && !key.isArrowKey() && !key.isModifierKey() && !key.isFunctionKey()
-//                    && !key.isNavigationKey() && !key.isMediaKey() && key != KeyCode.BACK_SPACE && !text.isEmpty()) {
-//                text = text.substring(0, length - 1);
-//                phoneField.clear();
-//                phoneField.setText(text);
-//                phoneField.positionCaret(length - 1);
+
+            // если удаляю последний символ слева, то если нахожусь на месте расделителей маски их тоже удаляю
+//            if (key == KeyCode.BACK_SPACE) {
+//                if (length == secondSpace) {
+//                    onBackSpaceFormatField(text, secondSpace);
+//                } else if (length == firstDash) {
+//                    onBackSpaceFormatField(text, firstDash);
+//                } else if (length == secondDash) {
+//                    onBackSpaceFormatField(text, secondDash);
+//                }
 //            }
 
-//            // ограничение на длину номера вместе со всеми символами
-//            if (length > fullPhoneLength && key.isDigitKey()) {
-//                text = text.substring(0, fullPhoneLength);
+            // после ввода в начале строки любого символа перепрыгиваю на ввод кода оператора
+//            if ((length == secondSpace + 1 || length == firstDash + 1 || length == secondDash + 1) && key == KeyCode.BACK_SPACE) {
+//                text = phoneField.getText();
+//                length = phoneField.getLength();
+//                text = text.substring(0, length - 2);
+//                length = text.length();
 //                phoneField.clear();
 //                phoneField.setText(text);
-//                phoneField.positionCaret(fullPhoneLength);
+//                phoneField.positionCaret(length);
 //            }
+
+            // todo чтото не так с удалением символов, начать со второго пробела
+            if (length == secondSpace + 1 && key == KeyCode.BACK_SPACE) {
+                text = phoneField.getText();
+                length = phoneField.getLength();
+                text = text.substring(0, length - 1);
+                length = text.length();
+                phoneField.clear();
+                phoneField.setText(text);
+                phoneField.positionCaret(length);
+            }
 
             // нажав вправо в пустом поле сразу перехожу к месту ввода кода оператора
             if (text.isEmpty() && key == KeyCode.RIGHT) {
@@ -162,13 +181,13 @@ public class Controller {
             }
 
             // после ввода в начале строки любого символа перепрыгиваю на ввод кода оператора
-            if (length==1 && key != KeyCode.BACK_SPACE) {
+            if (length == 1 && key != KeyCode.BACK_SPACE) {
                 setCursorToStartInput("");
             }
 
-            // todo тут что-то не так. не воспринимает цифры с цифровой панели нормально
-            if (length==1 && key.isDigitKey()) {
-                setCursorToStartInput(key.getChar());
+            // при взятии фокуса в поле с помощью таба сразу перепрыгиваю на ввод кода оператора
+            if (key == KeyCode.TAB) {
+                setCursorToStartInput("");
             }
 
             // находяся в начале ввода кода оператора нажав влево возвращаюсь к демонстрации всей маски телефона
@@ -178,7 +197,7 @@ public class Controller {
 
             // находясь в начале ввода кода оператора если удаляю пробел разделяющий код страны и код оператора,
             // то удаляю все и возвращаюсь к показу маски ввода телефона
-            if (phoneField.getText().equals(countryCode) && key == KeyCode.BACK_SPACE) {
+            if (length == firstSpace && key == KeyCode.BACK_SPACE) {
                 phoneField.clear();
             }
 
@@ -190,96 +209,7 @@ public class Controller {
             System.out.println("Number length: " + number.length());
         });
 
-//        phoneField.setOnKeyReleased(keyEvent -> {
-//            String text = phoneField.getText();
-//            int length = text.length();
-//
-//            CountryPhone countryPhone = countryMap.get(choiceBox.getValue());
-//            int secondSpace = countryPhone.getSecondSpace();
-//            int firstDash = countryPhone.getFirstDash();
-//            int secondDash = countryPhone.getSecondDash();
-//            int fullPhoneLength = countryPhone.getFullPhoneLength();
-//            String countryCode = countryPhone.getCountryCode();
-//
-//            KeyCode key = keyEvent.getCode();
-//
-//            // если ввожу цифры, то форматирую строку в нужных местах дополняя пробелами и тире
-//            if (key.isDigitKey() && !text.isEmpty()) {
-//                if (length == secondSpace) {
-//                    text = text.substring(0, secondSpace) + " " + text.substring(secondSpace);
-//                    length = text.length();
-//                    phoneField.clear();
-//                    phoneField.setText(text);
-//                    phoneField.positionCaret(length);
-//                } else if (length == firstDash) {
-//                    text = text.substring(0, firstDash) + "-" + text.substring(firstDash);
-//                    length = text.length();
-//                    phoneField.clear();
-//                    phoneField.setText(text);
-//                    phoneField.positionCaret(length);
-//                } else if (length == secondDash) {
-//                    text = text.substring(0, secondDash) + "-" + text.substring(secondDash);
-//                    length = text.length();
-//                    phoneField.clear();
-//                    phoneField.setText(text);
-//                    phoneField.positionCaret(length);
-//                }
-//            }
-//
-//            // если удаляю последний символ слева, то если нахожусь на месте расделителей маски их тоже удаляю
-//            if (key == KeyCode.BACK_SPACE) {
-//                if (length == secondSpace) {
-//                    onBackSpaceFormatField(text, secondSpace);
-//                } else if (length == firstDash) {
-//                    onBackSpaceFormatField(text, firstDash);
-//                } else if (length == secondDash) {
-//                    onBackSpaceFormatField(text, secondDash);
-//                }
-//            }
-//
-//            // если введена не цифра то удаляю ее
-//            if (!key.isDigitKey() && !key.isArrowKey() && !key.isModifierKey() && !key.isFunctionKey()
-//                    && !key.isNavigationKey() && !key.isMediaKey() && key != KeyCode.BACK_SPACE && !text.isEmpty()) {
-//                text = text.substring(0, length - 1);
-//                phoneField.clear();
-//                phoneField.setText(text);
-//                phoneField.positionCaret(length - 1);
-//            }
-//
-//            // ограничение на длину номера вместе со всеми символами
-//            if (length > fullPhoneLength && key.isDigitKey()) {
-//                text = text.substring(0, fullPhoneLength);
-//                phoneField.clear();
-//                phoneField.setText(text);
-//                phoneField.positionCaret(fullPhoneLength);
-//            }
-//
-//            // нажав вправо в пустом поле сразу перехожу к месту ввода кода оператора
-//            if (text.isEmpty() && key == KeyCode.RIGHT) {
-//                setCursorToStartInput();
-//            }
-//
-//            // находяся в начале ввода кода оператора нажав влево возвращаюсь к демонстрации всей маски телефона
-//            if (phoneField.getText().equals(countryCode + " ") && key == KeyCode.LEFT) {
-//                phoneField.clear();
-//            }
-//
-//            // находясь в начале ввода кода оператора если удаляю пробел разделяющий код страны и код оператора,
-//            // то удаляю все и возвращаюсь к показу маски ввода телефона
-//            if (phoneField.getText().equals(countryCode) && key == KeyCode.BACK_SPACE) {
-//                phoneField.clear();
-//            }
-//
-//            // отчищаю номер в поле от всех знаков
-//            String number = text.replaceAll("\\+", "").replaceAll(" ", "")
-//                    .replaceAll("-", "");
-//
-//            System.out.println("Number: " + number);
-//            System.out.println("Number length: " + number.length());
-//        });
-
         // при клике на поле ввода мышью заменяю маску на код страны и ставлю курсор в место начала ввода кода оператора
-
         phoneField.setOnMouseClicked(actionEvent -> setCursorToStartInput(""));
 
         // убрать фокус с поля ввода
@@ -304,15 +234,31 @@ public class Controller {
         phoneField.positionCaret(length);
     }
 
-//    UnaryOperator<TextFormatter.Change> filter = change -> {
-//        String text = change.getText();
-//
-//        if (text.matches("[0-9]*")) {
-//            return change;
-//        }
-//
-//        return null;
-//    };
-//    TextFormatter<String> textFormatter = new TextFormatter<>(filter);
+    private String namPadKeyHeader(KeyCode key) {
+        switch (key) {
+            case NUMPAD0:
+                return "0";
+            case NUMPAD1:
+                return "1";
+            case NUMPAD2:
+                return "2";
+            case NUMPAD3:
+                return "3";
+            case NUMPAD4:
+                return "4";
+            case NUMPAD5:
+                return "5";
+            case NUMPAD6:
+                return "6";
+            case NUMPAD7:
+                return "7";
+            case NUMPAD8:
+                return "8";
+            case NUMPAD9:
+                return "9";
+            default:
+                return "";
+        }
+    }
 
 }
